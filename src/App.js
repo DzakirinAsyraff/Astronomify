07350eab5d965e8fdb0bfd7c2b82bc9cf04828fe
Dzakirin Asyraff <dzakirinasyraff@gmail.com>
2022-10-04 09:47:09 +0800

    Reduce width of scroller in final page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import Home from './Pages/Home';
 import Main from './Pages/Main';
 import Final from './Pages/Final';
-import Test from './Pages/Test';
 import {Routes, Route} from 'react-router-dom';
 import './App.css';
 
@@ -10,7 +9,6 @@ function App() {
     <div className="App">
       <Routes>
         <Route path="/Astronomify" element={<Home />} />
-        <Route path="/Astronomify/test" element={<Test />} />
         <Route path="/Astronomify/main" element={<Main />} />
         <Route path="/Astronomify/final/:date" element={<Final />} />
       </Routes>
