2ab2b403f52f4cf12b8aa27a856541b0ce006b34
Dzakirin Asyraff <dzakirinasyraff@gmail.com>
2022-10-04 08:19:09 +0800

    Update home path

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
     <div className="App">
       <BrowserRouter>
       <Routes>
-        <Route path="/Astronomify/" element={<Home />} />
+        <Route path="/Astronomify" element={<Home />} />
         <Route path="/test" element={<Test />} />
         <Route path="/main" element={<Main />} />
         <Route path="/final/:date" element={<Final />} />
